d322e3098e25779d6f4beb72188f218c89b40a30
Johnsmith0508 <loganwaldman@gmail.com>
2016-01-06 08:35:50 -0500

    Revert "added a gui"
    This reverts commit 693f867651db9e57bc58f9af06c77a765aa2f381.

--- js/clientPhysics.js
+++ js/clientPhysics.js
@@ -3,78 +3,65 @@ var threeCube, cubeGeom, cubeMesh, planeMaterial, planeGeom, threePlane;
 
 
 var force = {
-  up: new CANNON.Vec3(0, 1, 0),
-  down: new CANNON.Vec3(0, -1, 0),
-  left: new CANNON.Vec3(0, 0, -1),
-  right: new CANNON.Vec3(0, 0, 1),
-  forward: new CANNON.Vec3(1, 0, 0),
-  back: new CANNON.Vec3(-1, 0, 0),
-  zero: new CANNON.Vec3(0, 0, 0)
+	up : new CANNON.Vec3(0,1,0),
+	down : new CANNON.Vec3(0,-1,0),
+	left : new CANNON.Vec3(0,0,-1),
+	right : new CANNON.Vec3(0,0,1),
+	forward : new CANNON.Vec3(1,0,0),
+	back : new CANNON.Vec3(-1,0,0),
+	zero : new CANNON.Vec3(0,0,0)
 }
 var initCannon = function() {
-  world = new CANNON.World();
-  world.gravity.set(0, -3.81, 0);
-  world.broadphase = new CANNON.NaiveBroadphase();
-  world.solver.iterations = 10;
-  groundShape = new CANNON.Plane();
-  ground = new CANNON.Body({
-    mass: 0
-  });
-  ground.addShape(groundShape);
-  ground.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
-  ground.position.set(0, -5, 0);
-  world.addBody(ground);
+	world = new CANNON.World();
+	world.gravity.set(0, -3.81, 0);
+	world.broadphase = new CANNON.NaiveBroadphase();
+	world.solver.iterations = 10;
+	groundShape = new CANNON.Plane();
+	ground = new CANNON.Body({
+		mass: 0
+	});
+	ground.addShape(groundShape);
+	ground.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
+	ground.position.set(0, -5, 0);
+	world.addBody(ground);
 }
 
 var initThree = function(scene) {
-  cubeGeom = new THREE.SphereGeometry(1);
-  testThing = new THREE.CubeGeometry(50, 50, 50);
-  cubeMesh = new THREE.MeshBasicMaterial({
-    color: 0xff0000,
-    wireframe: true
-  });
-  planeGeom = new THREE.PlaneGeometry(15, 15, 15, 15);
-  planeMesh = new THREE.MeshBasicMaterial({
-    color: 0xff00ff,
-    wireframe: true
-  });
-  threePlane = new THREE.Mesh(planeGeom, planeMesh);
+	cubeGeom = new THREE.SphereGeometry(1);
+	cubeMesh = new THREE.MeshBasicMaterial({
+		color: 0xff0000,
+		wireframe: true
+	});
+	planeGeom = new THREE.PlaneGeometry(15, 15, 15, 15);
+	planeMesh = new THREE.MeshBasicMaterial({
+		color: 0xff00ff,
+		wireframe: true
+	});
+	threePlane = new THREE.Mesh(planeGeom, planeMesh);
 
-  scene.add(threePlane);
+	scene.add(threePlane);
 }
 
 var upadtePhysics = function() {
-  world.step(1 / 60);
-  threePlane.position.copy(ground.position);
-  threePlane.quaternion.copy(ground.quaternion);
-  for (var i in user) {
-    user[i].updatePhis();
-  }
+	world.step(1 / 60);
+	threePlane.position.copy(ground.position);
+	threePlane.quaternion.copy(ground.quaternion);
+	for(var i in user) {
+		user[i].updatePhis();
+	}
 }
 
-var PhysicsSphere = function() {
-
-  //Protected Vars
-  THREE.Object3D.call(this);
-  this._sphere = new CANNON.Sphere(1);
-
-  //Public Vars
-  this.model = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  this.phisObj = new CANNON.Body({
-    mass: 1
-  });
-  this.phisMesh = new THREE.Mesh(cubeGeom, cubeMesh);
-  this.phisObj.addShape(this._sphere);
-  //functions
-  this.updatePhis = function() {
-    this.position.copy(this.phisObj.position);
-    this.phisMesh.position.copy(this.phisObj.position);
-    this.phisMesh.quaternion.copy(this.phisObj.quaternion);
-    this.model.quaternion.copy(this.phisObj.quaternion);
-  }
-  this.addText = function(text) {
-    addText(text, this);
-  }
+var addPhysicsCube = function(parent) {
+	var box = new CANNON.Sphere(1);
+	parent.phisObj = new CANNON.Body({mass:1});
+	parent.phisObj.addShape(box);
+	parent.reference = new THREE.Object3D();
+	parent.phisMesh = new THREE.Mesh(cubeGeom,cubeMesh);
+	
+	parent.updatePhis = function() {
+		this.position.copy(this.phisObj.position);
+		this.phisMesh.position.copy(this.phisObj.position);
+		this.phisMesh.quaternion.copy(this.phisObj.quaternion);
+		this.model.quaternion.copy(this.phisObj.quaternion);
+	}
 }
-PhysicsSphere.prototype = Object.create( THREE.Object3D.prototype );
-PhysicsSphere.prototype.constructor = PhysicsSphere;
