8c7163504dfe89d833d9339bb3113ad0dc49a317
Johnsmith0508 <loganwaldman@gmail.com>
2016-01-14 10:51:32 -0500

    docs again

--- js/GUI.js
+++ js/GUI.js
@@ -1,23 +1,56 @@
 /*global THREE */
 if (typeof THREE === "undefined") throw "THREE.JS not found";
+
+/** @namespace */
 var GUI = {
 	version: '0.0.1'
 };
+/**
+center of the scene
+*/
 GUI.origin = new THREE.Vector3(0, 0, 0);
+/**
+@constructor
+*/
 GUI.guiScene = function() {
+	/** the camera that is used
+	@private */
 	this.camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, -500, 1000);
+	/** 
+	the actual scene that is rendered
+	@deprecated use {@link GUI.guiScene.addElement} instead
+	*/
 	this.scene = new THREE.Scene();
 	this.scene.add(this.camera);
 	this.camera.position.z = 50;
 	this.camera.lookAt(GUI.origin);
-
+	/**
+	add an element to the gui
+	@function
+	@param {Object3D} object - object to add to the scene
+	@returns {Object3D} the passed object
+	*/
 	this.addElement = function(object) {
 		this.scene.add(object);
 		return object;
 	}
+	/**
+	render the scene
+	@function
+	@param renderer - the THREE.js renderer that is used to render this
+	*/
 	this.render = function(renderer) {
 		renderer.render(this.scene, this.camera);
 	}
+	/**
+	add text to the gui
+	@param {String} text - the text to be added
+	@param {Int} x - x position (0,0 is center of screen)
+	@param {Int} y - y position (0,0 is center of screen)
+	@param {Object} [opts] - extra options to be passed
+	@param {Int} [opts.z] - z-index of the object
+	@returns {Object3D} the object that contains the text
+	*/ 
 	this.addTextElement = function(text, x, y, opts) {
 		
 		if(typeof opts === "undefined") opts = {};
