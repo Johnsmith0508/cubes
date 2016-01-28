f1159f442eb62b4346c420e5c6ea997906baea81
Johnsmith0508 <loganwaldman@gmail.com>
2016-01-28 08:51:32 -0500

    refactored the functions.js file now it is just functions that help by existing

--- js/client.js
+++ js/client.js
@@ -1,21 +1,8 @@
 /*global $ THREE force initThree updatePhysics world CapsuleColider*/
 
-/**
- * house keeping var; not used in code
- * @deprecated
- * @private
- */
 var usedConsoleLogs = /^((?!\/\/).)*console\.log*/gi;
-/**  renderer variable */
-var scene,guiScene , camera, renderer, objMtlLoader, JsonLoader, gui;
-/** app spesific variable */
-var chatHideDelay, userName = "", isShifted,blendMesh,testsprite;
-/** client model vars */
-var cannonDebugRenderer;
-var cubeGeometry, cubeMaterial, clientCubeMaterial;
-var carGeometry, carMaterial,controls;
-/** vars for enviroment */
-var floorMaterial, wallsMaterial, light;
+var scene, guiScene, camera, renderer, objMtlLoader, JsonLoader, gui, chatHideDelay, userName = "",
+	isShifted, blendMesh, testsprite, cannonDebugRenderer, cubeGeometry, cubeMaterial, clientCubeMaterial, carGeometry, carMaterial, controls, floorMaterial, wallsMaterial, light;
 //create socket.io connection to server
 var socket = new io('//dynalogic.org', {
 	path: '/node/socket.io'
@@ -39,12 +26,211 @@ var key = {
 /** Object of all users @private */
 var user = {};
 //so the server is only told about no keys being pressed once
-var sentUpdateNoKey = true;
+var sendUpdateNoKey = true;
+var directonalForce = new CANNON.Vec3(0, 0, 0);
+var jumpForce = new CANNON.Vec3(0, 10, 0);
+var canJump = true;
+
+//handles the sending of keys to the server
+var buttonHandler = function(keyPressed, status) {
+	if (keyPressed.target === $(".chat")) return;
+	switch (keyPressed.which) {
+		case 87:
+			key.w = status;
+			break;
+		case 83:
+			key.s = status;
+			break;
+		case 65:
+			key.a = status;
+			break;
+		case 68:
+			key.d = status;
+			break;
+		case 81:
+			key.q = status;
+			break;
+		case 69:
+			key.e = status;
+			break;
+		case 32:
+			key.space = status;
+			break;
+		case 84:
+			key.t = status;
+			break;
+		case 82:
+			controls.reset();
+			break;
+	}
+	if (keyPressed.shiftKey) {
+		key.shift = true;
+	} else {
+		key.shift = false;
+	}
+}
+var registerEvents = function() {
+
+		//called when a user joins the server
+		socket.on('user joined', function(data) {
+			if (typeof user[data.name] == "undefined" && typeof userName != "undefined") {
+				//userName = data.name;
+				//user[data.name] = new PhysicsSphere();
+				user[data.name] = new CapsuleColider(1, 4, data.name);
+				user[data.name].position.fromArray(data.position);
+				user[data.name].rotation.fromArray(data.rotation);
+				world.addBody(user[data.name].phisObj);
+				scene.add(user[data.name].phisMesh);
+				scene.add(user[data.name]);
+				//user[data.name].addText(data.name);
+			}
+		});
+		//sent when a user leaves
+		socket.on('user left', function(name) {
+			//console.log(name);
+			scene.remove(user[name]);
+			scene.remove(user[name].phisMesh);
+			world.removeBody(user[name].phisObj);
+			delete user[name];
+		});
+		//meh
+		socket.on('physics change', function(data) {
+			if (typeof user[userName] !== "undefined") {
+				//console.log(data);
+				user[data.name].phisObj.position.set(data.position.x, data.position.y, data.position.z);
+				user[data.name].phisObj.velocity.set(data.velocity.x, data.velocity.y, data.velocity.z);
+				user[data.name].phisObj.quaternion.set(data.quaternion.x, data.quaternion.y, data.quaternion.z, data.quaternion.w);
+			}
+		});
+		socket.on('error', function(error) {
+			if (error == "user exists") {
+				$("#ErrorMsg").val("Username is in use, Try annother");
+				$("#ErrorMsg").show();
+				registerSubmitButton();
+			}
+		});
+		socket.on('user created', function() {
+			//console.log('meh');
+			preInit();
+		});
+		socket.on('latencyCheck', function(oldTime) {
+			var time = new Date().getTime();
+			$("#pingDisplay").text(Math.floor((time - oldTime) / 2) + "ms");
+		});
+	}
+	//handles the logic behind the submit button on the login screen
+var submitHandler = function() {
+	$('#name').off('keyup');
+	registerEvents();
+	registerChatSocket();
+	if ($("#name").val().length > 0) {
+		modelType = $(".model:checked").val();
+		socket.emit('create user', {
+			name: $("#name").val(),
+			model: modelType,
+			cookie: getCookie('login')
+		});
+	}
+}
+
+//call to enable the submit button on the main page
+var registerSubmitButton = function() {
+	$("#sendName").one('click', submitHandler);
+	$('#name').on('keyup', function(e) {
+		if (e.keyCode === 13) {
+			$('#sendName').trigger('click');
+		}
+	});
+}
+
+//function that contains all logic for various things
+var mainLoop = function() {
+	key.angle = controls.getAzimuthalAngle();
+	directonalForce.setZero();
+	if (key.w) directonalForce.add(-Math.sin(key.angle), 0, -Math.cos(key.angle));
+	if (key.s) directonalForce.add(Math.sin(key.angle), 0, Math.cos(key.angle));
+	if (key.a) directonalForce.add(-Math.sin(key.angle + Math.PI / 2), 0, -Math.cos(key.angle + Math.PI / 2));
+	if (key.d) directonalForce.add(Math.sin(key.angle + Math.PI / 2), 0, Math.cos(key.angle + Math.P / 2));
+	if (key.space && canJump) {
+		user[userName].phisObj.applyImpulse(jumpForce, user[userName].phisObj.position);
+		canJump = false;
+	}
+	if (key.shift) directonalForce.add(0, -0.25, 0);
+	directonalForce.normalize();
+	//if (key.q) user[userName].rotation.y += 0.1;
+	//if (key.e) user[userName].rotation.y -= 0.1;
+	//if (key.space) user[userName].phisObj.applyImpulse(force.up, user[userName].phisObj.position);
+	//if (key.shift) user[userName].phisObj.applyImpulse(force.down, user[userName].phisObj.position);
+
+	if (key.w || key.a || key.s || key.d || key.q || key.e || key.space || key.shift) {
+		user[userName].phisObj.applyImpulse(directonalForce, user[userName].phisObj.position);
+		sendUpdateNoKey = true;
+		socket.emit('keys pressed', key);
+	} else if (userName.length > 0) {
+		user[userName].phisObj.velocity.x *= 0.75;
+		user[userName].phisObj.velocity.z *= 0.75;
+		if (sendUpdateNoKey) {
+			sendUpdateNoKey = false;
+			socket.emit('keys pressed', key);
+		}
+	}
+}
+
+var preInit = function() {
+	//console.log('preinit start');
+	userName = $("#name").val();
+	$('#login').hide();
+	$('#main_window').show();
+	chatHideDelay = $("#chatDelay").val();
+	//addText("test",camera);
+	$(document).on('keydown', function(e) {
+		buttonHandler(e, true);
+	});
+	$(document).on('keyup', function(e) {
+		buttonHandler(e, false);
+	});
+	user[userName] = new CapsuleColider(1, 4);
+	switch (modelType) {
+		case "car":
+			scene.add(camera);
+			user[userName].model = new THREE.Mesh(carGeometry, carMaterial);
+			user[userName].model.scale.set(0.6, 0.6, 0.6);
+			user[userName].model.rotateX(-Math.PI / 2);
+			user[userName].model.rotateZ(Math.PI);
+			user[userName].updateMatrixWorld();
+			user[userName].add(user[userName].model);
+			THREE.SceneUtils.attach(camera, scene, user[userName]);
+			break;
+		case "person":
+			user[userName] = new THREE.Mesh(blendMesh.geometry, blendMesh.material);
+			user[userName].scale.set(0.01, 0.01, 0.01);
+			scene.add(user[userName]);
+			user[userName].add(camera);
+			break;
+		default:
+			user[userName].add(camera);
+	}
+	scene.add(user[userName]);
+	world.addBody(user[userName].phisObj);
+	//scene.add(user[userName].phisMesh);
+	var nameGuiElement = gui.addTextElement(userName, window.innerWidth / -2, window.innerHeight / 2 - 20, {
+		textColor: "#262626"
+	});
+
+	nameGuiElement.position.x += nameGuiElement.scale.x / 2;
+	$("#threeJsRenderWindow").append(renderer.domElement);
+	if ($("#fpsShow").is(":checked")) {
+		document.body.appendChild(stats.domElement);
+	} else {
+		registerSubmitButton();
+	}
+}
+
 //initilise all required variables
 /**
-* Init function
-* @function
-*/
+ * Init function
+ * @function
+ */
 function init() {
 	//create three.js scene / init loaders
 	scene = new THREE.Scene();
@@ -99,7 +285,7 @@ function init() {
 	plane.reciveShadow = true;
 	//scene.add(plane);
 
-	var test = new THREE.Mesh(cubeGeometry,cubeMaterial);
+	var test = new THREE.Mesh(cubeGeometry, cubeMaterial);
 	//test.position;
 	//gui.addElement(test);
 	//testsprite = gui.addTextElement("hello", 50,250);
@@ -107,20 +293,22 @@ function init() {
 	JsonLoader.load('/node/model/car.AnExtention', function(loadedCar) {
 		carGeometry = loadedCar;
 	});
-	blendMesh.load('model/marine_anims.js',function(){
-		blendMesh.scale.set(0.01,0.01,0.01);
+	blendMesh.load('model/marine_anims.js', function() {
+		blendMesh.scale.set(0.01, 0.01, 0.01);
 		//guiScene.add(blendMesh);
 	});
-	
+
 	initThree(scene);
 	initCannon();
-	
-	cannonDebugRenderer = new THREE.CannonDebugRenderer( scene, world );
+
+	cannonDebugRenderer = new THREE.CannonDebugRenderer(scene, world);
 	//init renderer
-	renderer = new THREE.WebGLRenderer({alpha:true});
+	renderer = new THREE.WebGLRenderer({
+		alpha: true
+	});
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	renderer.autoClear = false;
-	controls = new THREE.OrbitControls( camera, renderer.domElement );
+	controls = new THREE.OrbitControls(camera, renderer.domElement);
 	//controls.addEventListener( 'change', render ); // add this only if there is no animation loop (requestAnimationFrame)
 	controls.enableDamping = true;
 	controls.dampingFactor = 0.25;
@@ -129,8 +317,8 @@ function init() {
 
 }
 /**
-* Function to render the scene(s)
-*/
+ * Function to render the scene(s)
+ */
 function animate() {
 	stats.begin();
 	mainLoop();
@@ -148,56 +336,7 @@ socket.on('user count', function(users) {
 	$("#usersOnline").text(users + " online currently");
 });
 //socket.on(*), part of a failed project
-var registerEvents = function() {
-	
-		//called when a user joins the server
-		socket.on('user joined', function(data) {
-			if (typeof user[data.name] == "undefined" && typeof userName != "undefined") {
-        //userName = data.name;
-				//user[data.name] = new PhysicsSphere();
-				user[data.name] = new CapsuleColider(1,4,data.name);
-				user[data.name].position.fromArray(data.position);
-				user[data.name].rotation.fromArray(data.rotation);
-				world.addBody(user[data.name].phisObj);
-				scene.add(user[data.name].phisMesh);
-				scene.add(user[data.name]);
-				//user[data.name].addText(data.name);
-			}
-		});
-		//sent when a user leaves
-		socket.on('user left', function(name) {
-			//console.log(name);
-			scene.remove(user[name]);
-			scene.remove(user[name].phisMesh);
-			world.removeBody(user[name].phisObj);
-			delete user[name];
-		});
-		//meh
-		socket.on('physics change',function(data){
-			if(typeof user[userName] !== "undefined"){
-				//console.log(data);
-				user[data.name].phisObj.position.set(data.position.x,data.position.y,data.position.z);
-				user[data.name].phisObj.velocity.set(data.velocity.x,data.velocity.y,data.velocity.z);
-				user[data.name].phisObj.quaternion.set(data.quaternion.x,data.quaternion.y,data.quaternion.z,data.quaternion.w);
-			}
-		});
-		socket.on('error',function(error){
-			if(error == "user exists")
-				{
-					$("#ErrorMsg").val("Username is in use, Try annother");
-					$("#ErrorMsg").show();
-					registerSubmitButton();
-				}
-		});
-		socket.on('user created',function(){
-			//console.log('meh');
-			preInit();
-		});
-		socket.on('latencyCheck',function(oldTime){
-			var time = new Date().getTime();
-			$("#pingDisplay").text(Math.floor((time - oldTime)/2) + "ms");
-		});
-	}
+
 //run init
 init();
 //begin rendering
@@ -209,9 +348,9 @@ $(function() {
 		$("#options").toggle();
 	});
 });
-setInterval(function(){
-	socket.emit('latencyCheck',new Date().getTime());
-},1000);
+setInterval(function() {
+	socket.emit('latencyCheck', new Date().getTime());
+}, 1000);
 
 //handle window changing size
 function onWindowResize() {
@@ -219,7 +358,7 @@ function onWindowResize() {
 	windowHalfY = window.innerHeight / 2;
 	camera.aspect = window.innerWidth / window.innerHeight;
 	camera.updateProjectionMatrix();
-	
+
 	//GUI.camera.updateProjectionMatrix();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 }
--- js/functions.js
+++ js/functions.js
@@ -1,176 +1,72 @@
 /*global key CANNON registerChatSocket controls renderer user world gui $ */
 var getCookies = function() {
-	var c = document.cookie,
-		v = 0,
-		cookies = {};
-	if (document.cookie.match(/^\s*\$Version=(?:"1"|1);\s*(.*)/)) {
-		c = RegExp.$1;
-		v = 1;
-	}
-	if (v === 0) {
-		c.split(/[,;]/).map(function(cookie) {
-			var parts = cookie.split(/=/, 2),
-				name = decodeURIComponent(parts[0].trimLeft()),
-				value = parts.length > 1 ? decodeURIComponent(parts[1].trimRight()) : null;
-			cookies[name] = value;
-		});
-	} else {
-		c.match(/(?:^|\s+)([!#$%&'*+\-.0-9A-Z^`a-z|~]+)=([!#$%&'*+\-.0-9A-Z^`a-z|~]*|"(?:[\x20-\x7E\x80\xFF]|\\[\x00-\x7F])*")(?=\s*[,;]|$)/g).map(function($0, $1) {
-			var name = $0,
-				value = $1.charAt(0) === '"' ? $1.substr(1, -1).replace(/\\(.)/g, "$1") : $1;
-			cookies[name] = value;
-		});
-	}
-	return cookies;
+  var c = document.cookie,
+    v = 0,
+    cookies = {};
+  if (document.cookie.match(/^\s*\$Version=(?:"1"|1);\s*(.*)/)) {
+    c = RegExp.$1;
+    v = 1;
+  }
+  if (v === 0) {
+    c.split(/[,;]/).map(function(cookie) {
+      var parts = cookie.split(/=/, 2),
+        name = decodeURIComponent(parts[0].trimLeft()),
+        value = parts.length > 1 ? decodeURIComponent(parts[1].trimRight()) : null;
+      cookies[name] = value;
+    });
+  } else {
+    c.match(/(?:^|\s+)([!#$%&'*+\-.0-9A-Z^`a-z|~]+)=([!#$%&'*+\-.0-9A-Z^`a-z|~]*|"(?:[\x20-\x7E\x80\xFF]|\\[\x00-\x7F])*")(?=\s*[,;]|$)/g).map(function($0, $1) {
+      var name = $0,
+        value = $1.charAt(0) === '"' ? $1.substr(1, -1).replace(/\\(.)/g, "$1") : $1;
+      cookies[name] = value;
+    });
+  }
+  return cookies;
 }
 
 function getCookie(name) {
-	return getCookies()[name];
+  return getCookies()[name];
 }
 
-
-var sendUpdateNoKey = false;
-var directonalForce = new CANNON.Vec3(0, 0, 0);
-var jumpForce = new CANNON.Vec3(0,10,0);
-var canJump = true;
-
-//handles the sending of keys to the server
-var buttonHandler = function(keyPressed, status) {
-    if (keyPressed.target === $(".chat")) return;
-    switch (keyPressed.which) {
-      case 87:
-        key.w = status;
-        break;
-      case 83:
-        key.s = status;
-        break;
-      case 65:
-        key.a = status;
-        break;
-      case 68:
-        key.d = status;
-        break;
-      case 81:
-        key.q = status;
-        break;
-      case 69:
-        key.e = status;
-        break;
-      case 32:
-        key.space = status;
-        break;
-      case 84:
-        key.t = status;
-        break;
-      case 82:
-        controls.reset();
-        break;
-    }
-    if (keyPressed.shiftKey) {
-      key.shift = true;
-    } else {
-      key.shift = false;
-    }
-  }
-  //handles the logic behind the submit button on the login screen
-var submitHandler = function() {
-    $('#name').off('keyup');
-    registerEvents();
-    registerChatSocket();
-    if ($("#name").val().length > 0) {
-      modelType = $(".model:checked").val();
-      socket.emit('create user', {
-        name: $("#name").val(),
-        model: modelType,
-        cookie: getCookie('login')
-      });
-    }
-  }
-  //call to enable the submit button on the main page
-var registerSubmitButton = function() {
-  $("#sendName").one('click', submitHandler);
-  $('#name').on('keyup', function(e) {
-    if (e.keyCode === 13) {
-      $('#sendName').trigger('click');
-    }
+/*global THREE $ */
+function roundRect(ctx, x, y, w, h, r) {
+  ctx.beginPath();
+  ctx.moveTo(x + r, y);
+  ctx.lineTo(x + w - r, y);
+  ctx.quadraticCurveTo(x + w, y, x + w, y + r);
+  ctx.lineTo(x + w, y + h - r);
+  ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h);
+  ctx.lineTo(x + r, y + h);
+  ctx.quadraticCurveTo(x, y + h, x, y + h - r);
+  ctx.lineTo(x, y + r);
+  ctx.quadraticCurveTo(x, y, x + r, y);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+}
+var addText = function(text, parentObject) {
+  var canvas = document.createElement('canvas'),
+    canvasContext = canvas.getContext('2d');
+  canvas.style.border = '3px solid #000';
+  canvas.style.borderRadius = '15px';
+  canvas.height = 105;
+  canvasContext.font = "100px Arial";
+  var textWidth = canvasContext.measureText(text).width;
+  canvas.width = textWidth;
+  canvasContext.font = "normal 100px Arial";
+  canvasContext.fillStyle = "#fff";
+  roundRect(canvasContext, 1, 1, textWidth - 2, 100, 8);
+  canvasContext.fillStyle = "#000";
+  canvasContext.fillText(text, 0, 85);
+  var texture = new THREE.Texture(canvas);
+  texture.needsUpdate = true;
+  var material = new THREE.SpriteMaterial({
+    map: texture
   });
-}
-
-var preInit = function() {
-    //console.log('preinit start');
-    userName = $("#name").val();
-    $('#login').hide();
-    $('#main_window').show();
-    chatHideDelay = $("#chatDelay").val();
-    //addText("test",camera);
-    $(document).on('keydown', function(e) {
-      buttonHandler(e, true);
-    });
-    $(document).on('keyup', function(e) {
-      buttonHandler(e, false);
-    });
-    user[userName] = new CapsuleColider(1, 4);
-    switch (modelType) {
-      case "car":
-        scene.add(camera);
-        user[userName].model = new THREE.Mesh(carGeometry, carMaterial);
-        user[userName].model.scale.set(0.6, 0.6, 0.6);
-        user[userName].model.rotateX(-Math.PI / 2);
-        user[userName].model.rotateZ(Math.PI);
-        user[userName].updateMatrixWorld();
-        user[userName].add(user[userName].model);
-        THREE.SceneUtils.attach(camera, scene, user[userName]);
-        break;
-      case "person":
-        user[userName] = new THREE.Mesh(blendMesh.geometry, blendMesh.material);
-        user[userName].scale.set(0.01, 0.01, 0.01);
-        scene.add(user[userName]);
-        user[userName].add(camera);
-        break;
-      default:
-        user[userName].add(camera);
-    }
-    scene.add(user[userName]);
-    world.addBody(user[userName].phisObj);
-    //scene.add(user[userName].phisMesh);
-    var nameGuiElement = gui.addTextElement(userName, window.innerWidth / -2, window.innerHeight / 2 - 20, {
-      textColor: "#262626"
-    });
-
-    nameGuiElement.position.x += nameGuiElement.scale.x / 2;
-    $("#threeJsRenderWindow").append(renderer.domElement);
-    if ($("#fpsShow").is(":checked")) {
-      document.body.appendChild(stats.domElement);
-    } else {
-      registerSubmitButton();
-    }
-  }
-  //function that contains all logic for various things
-var mainLoop = function() {
-  key.angle = controls.getAzimuthalAngle();
-  directonalForce.setZero();
-  if (key.w) directonalForce.add(-Math.sin(key.angle), 0, -Math.cos(key.angle));
-  if (key.s) directonalForce.add(Math.sin(key.angle), 0, Math.cos(key.angle));
-  if (key.a) directonalForce.add(-Math.sin(key.angle + Math.PI / 2), 0, -Math.cos(key.angle + Math.PI / 2));
-  if (key.d) directonalForce.add(Math.sin(key.angle + Math.PI / 2), 0, Math.cos(key.angle + Math.P / 2));
-  if (key.space && canJump) {user[userName].phisObj.applyImpulse(jumpForce,user[userName].phisObj.position); canJump = false;}
-  if (key.shift) directonalForce.add(0, -0.25, 0);
-  directonalForce.normalize();
-  //if (key.q) user[userName].rotation.y += 0.1;
-  //if (key.e) user[userName].rotation.y -= 0.1;
-  //if (key.space) user[userName].phisObj.applyImpulse(force.up, user[userName].phisObj.position);
-  //if (key.shift) user[userName].phisObj.applyImpulse(force.down, user[userName].phisObj.position);
-
-  if (key.w || key.a || key.s || key.d || key.q || key.e || key.space || key.shift) {
-    user[userName].phisObj.applyImpulse(directonalForce, user[userName].phisObj.position);
-    sendUpdateNoKey = true;
-    socket.emit('keys pressed', key);
-  } else if (userName.length > 0) {
-    user[userName].phisObj.velocity.x *= 0.75;
-    user[userName].phisObj.velocity.z *= 0.75;
-    if (sendUpdateNoKey) {
-      sendUpdateNoKey = false;
-      socket.emit('keys pressed', key);
-    }
-  }
+  var sprite = new THREE.Sprite(material);
+  sprite.scale.set(textWidth / 100, 1, 1);
+  parentObject.add(sprite);
+  sprite.position.z += 2.5;
+  console.log('1');
+  return sprite;
 }
